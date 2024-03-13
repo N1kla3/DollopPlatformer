80f2477c6bbc724060ae76d083ea5662a89a4295
kolya <kolya.vladimirsky@gmail.com>
2024-03-13 23:11:30 +0300

    Changes to scene

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,11 +1,36 @@
-extends Node2D
+extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+const MOVELEFT = 'move_left'
+const MOVERIGHT = 'move_right'
+
+const MOVEUP = 'move_up'
+const MOVEDOWN = 'move_down'
+
+@onready var animation_sprite = $AnimatedSprite2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	# if not is_on_floor():
+	# velocity += get_gravity() * delta
+
+	var direction = Input.get_vector(MOVELEFT, MOVERIGHT, MOVEUP, MOVEDOWN)
+	velocity = SPEED * direction
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	# var direction := Input.get_axis("ui_left", "ui_right")
+	# if direction:
+		# velocity.x = direction * SPEED
+	# else:
+		# velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
