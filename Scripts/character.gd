3975039392fb394be1c833a23fa88293cb467467
kolya <kolya.vladimirsky@gmail.com>
2024-03-16 13:38:30 +0300

    Add platforms and jump

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+class_name Platform_character
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -10,19 +11,26 @@ const MOVERIGHT = 'move_right'
 const MOVEUP = 'move_up'
 const MOVEDOWN = 'move_down'
 
+const JUMP = 'jump'
+
+@export var animation : SpriteFrames
+
 @onready var animation_sprite = $AnimatedSprite2D
+
+func _ready() -> void:
+	animation_sprite.sprite_frames = animation
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-	# velocity += get_gravity() * delta
 
 	var direction = Input.get_vector(MOVELEFT, MOVERIGHT, MOVEUP, MOVEDOWN)
-	velocity = SPEED * direction
+	velocity.x = SPEED * direction.x
+
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed(JUMP) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
