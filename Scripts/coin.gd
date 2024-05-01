d18450ee82686f09e93017f303d5b2011a71b109
kolya <kolya.vladimirsky@gmail.com>
2024-05-01 23:36:48 +0300

    coin sprites in editor

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -1,9 +1,24 @@
+@tool
 extends Area2D
 
 class_name Coin
 
-var score_value : int = 10
-@export var animation : SpriteFrames
+@export var score_value : int = 10
+@export var animation : SpriteFrames:
+	set (new_sprites):
+		animation = new_sprites
+		if Engine.is_editor_hint() && $AnimatedSprite2D:
+			$AnimatedSprite2D.sprite_frames = new_sprites
+			$AnimatedSprite2D.play("Flip")
+
+@export var collision_size : int :
+	set (new_size):
+		collision_size = new_size
+		if Engine.is_editor_hint() && $CollisionShape2D:
+			var temp_shape = $CollisionShape2D.shape as RectangleShape2D
+			temp_shape.size.x = new_size;
+			temp_shape.size.y = new_size;
+			
 
 func _ready() -> void:
 	body_entered.connect(on_body_entered)
@@ -14,7 +29,7 @@ func on_body_entered(body: Node2D):
 	print("body entered", body.name)
 	var temp = body as Platform_character
 	if temp:
-		temp.score += 110
+		temp.score += score_value
 	hide()
 	collision_layer = 0x00
 	collision_mask = 0x00
