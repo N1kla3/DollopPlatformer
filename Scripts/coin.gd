108994a1d2faff4dc0b9b102697091852546124a
Nicolas <kolya.vladimirsky@gmail.com>
2025-01-06 22:52:04 +0300

    add life spheres

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -3,13 +3,15 @@ extends Area2D
 
 class_name Coin
 
+
+const animation_name = "Idle"
 @export var score_value : int = 10
 @export var animation : SpriteFrames:
 	set (new_sprites):
 		animation = new_sprites
 		if Engine.is_editor_hint() && $AnimatedSprite2D:
 			$AnimatedSprite2D.sprite_frames = new_sprites
-			$AnimatedSprite2D.play("Flip")
+			$AnimatedSprite2D.play(animation_name)
 
 @export var collision_size : int :
 	set (new_size):
@@ -23,15 +25,18 @@ class_name Coin
 func _ready() -> void:
 	body_entered.connect(on_body_entered)
 	%AnimatedSprite2D.sprite_frames = animation	
-	%AnimatedSprite2D.play("Flip")
+	%AnimatedSprite2D.play(animation_name)
+	var temp_shape = $CollisionShape2D.shape as RectangleShape2D
+	temp_shape.size.x = collision_size;
+	temp_shape.size.y = collision_size;
 
 func on_body_entered(body: Node2D):
 	print("body entered", body.name)
 	var temp = body as Platform_character
 	if temp:
 		temp.score += score_value
-	hide()
-	collision_layer = 0x00
-	collision_mask = 0x00
-	queue_free()
+		hide()
+		collision_layer = 0x00
+		collision_mask = 0x00
+		queue_free()
 	
