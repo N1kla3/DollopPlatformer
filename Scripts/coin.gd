8ba354ddd94a22c5321a3fab2e119ef9ea51aee9
Nicolas <kolya.vladimirsky@gmail.com>
2025-01-08 21:20:43 +0300

    Fix console errors on coin initialization

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -5,29 +5,34 @@ class_name Coin
 
 
 const animation_name = "Idle"
+@onready var animation_sprite : AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision_shape : CollisionShape2D = $CollisionShape2D
 @export var score_value : int = 10
 @export var animation : SpriteFrames:
 	set (new_sprites):
+		print("set animation coin")
 		animation = new_sprites
-		if Engine.is_editor_hint() && $AnimatedSprite2D:
-			$AnimatedSprite2D.sprite_frames = new_sprites
-			$AnimatedSprite2D.play(animation_name)
+		if Engine.is_editor_hint() && animation_sprite:
+			animation_sprite.sprite_frames = new_sprites
+			animation_sprite.play(animation_name)
 
 @export var collision_size : int :
 	set (new_size):
+		print("set collision size coin")
 		collision_size = new_size
-		if Engine.is_editor_hint() && $CollisionShape2D:
-			var temp_shape = $CollisionShape2D.shape as RectangleShape2D
+		if Engine.is_editor_hint() && collision_shape:
+			var temp_shape = collision_shape.shape as RectangleShape2D
 			if temp_shape:
 				temp_shape.size.x = new_size;
 				temp_shape.size.y = new_size;
 			
 
 func _ready() -> void:
+	print("ready coin")
 	body_entered.connect(on_body_entered)
-	%AnimatedSprite2D.sprite_frames = animation	
-	%AnimatedSprite2D.play(animation_name)
-	var temp_shape = $CollisionShape2D.shape as RectangleShape2D
+	animation_sprite.sprite_frames = animation	
+	animation_sprite.play(animation_name)
+	var temp_shape = collision_shape.shape as RectangleShape2D
 	if temp_shape:
 		temp_shape.size.x = collision_size;
 		temp_shape.size.y = collision_size;
--- Scripts/life_sphere.gd
+++ Scripts/life_sphere.gd
@@ -9,10 +9,4 @@ func apply_sphere(character : Platform_character) -> void:
 	character.health += 25;
 
 func _ready() -> void:
-	body_entered.connect(on_body_entered)
-	%AnimatedSprite2D.sprite_frames = animation	
-	%AnimatedSprite2D.play(animation_name)
-	var temp_shape = $CollisionShape2D.shape as RectangleShape2D
-	if temp_shape:
-		temp_shape.size.x = collision_size;
-		temp_shape.size.y = collision_size;
+	super()
