c04bde6370791cca0029a93a599f1a08b8b05d9e
Nicolas <kolya.vladimirsky@gmail.com>
2025-02-06 22:45:33 +0300

    Delete unused attribute compo

--- Scripts/attribute_asset.gd
+++
@@ -1,9 +0,0 @@
-extends Resource
-
-class_name AttributeAsset
-
-@export var health : int
-@export var max_health : int
-@export var speed : int
-@export var max_speed : int
-@export var damage : int
